12a16f796a09772b83587e9e80480d5028ad51fe
Keith Smiley <keithbsmiley@gmail.com>
2022-10-13 18:58:19 -0700

    Add release workflow (#143)
    This workflow is manually triggered, takes a tag name, and creates a
    release. Before tagging the release it pushes an update to the
    MODULE.bazel file. GitHub Actions don't allow multi-line text input, so
    it creates the release with TODOs that need to be filled out after it is
    created. This still saves quite a few steps in the process.

---
+++ .github/generate-notes.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+
+readonly new_version=$1
+readonly release_archive="apple_support.$new_version.tar.gz"
+
+sha=$(shasum -a 256 "$release_archive" | cut -d " " -f1)
+
+cat <<EOF
+## What's Changed
+
+TODO
+
+This release is compatible with: TODO
+
+## Workspace Snippet
+
+\`\`\`bzl
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+http_archive(
+    name = "build_bazel_apple_support",
+    sha256 = "$sha",
+    url = "https://github.com/bazelbuild/apple_support/releases/download/$new_version/apple_support.$new_version.tar.gz",
+)
+
+load(
+    "@build_bazel_apple_support//lib:repositories.bzl",
+    "apple_support_dependencies",
+)
+
+apple_support_dependencies()
+\`\`\`
+EOF
