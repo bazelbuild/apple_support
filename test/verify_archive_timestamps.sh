e77a7936aa799b522e23fecf3c2b592acbc3ed4a
Keith Smiley <keithbsmiley@gmail.com>
2023-08-21 15:16:15 -0700

    Move test fixtures to have unique names
    Otherwise we get a warning when running tests

--- test/verify_archive_timestamps.sh
+++ test/verify_archive_timestamps.sh
@@ -16,8 +16,8 @@ ar -tv "$binary"
 # Dec 31 1969 or Jan 1 1970 -- either is fine.
 # We would use 'date' here, but the format is slightly different (Jan 1 vs.
 # Jan 01).
-if ! ar -tv "$binary" | grep "lib" | grep "Dec 31" | grep "1969" \
-  && ! ar -tv "$binary" | grep "lib" | grep "Jan  1" | grep "1970"
+if ! ar -tv "$binary" | grep "objc_lib" | grep "Dec 31" | grep "1969" \
+  && ! ar -tv "$binary" | grep "objc_lib" | grep "Jan  1" | grep "1970"
 then
   echo "error: timestamp of contents of archive file should be zero" >&2
   exit 1
