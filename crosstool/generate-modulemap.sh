4cd7ccfae6fdaaca75f76b7dedfe4e3825cddaff
Keith Smiley <keithbsmiley@gmail.com>
2024-04-23 16:27:59 -0400

    Only do Contents/Developer
    There's really not much outside of this dir, and I'm not sure any of it
    is meant for public consumption. This wouldn't work unless I edited
    DEVELOPER_DIR in wrapped_clang too which feels bad

--- crosstool/generate-modulemap.sh
+++ crosstool/generate-modulemap.sh
@@ -6,7 +6,7 @@ cd "$(xcode-select -p)"
 
 echo 'module "crosstool" [system] {'
 
-find .. -type f \( -name "*.h" -o -name "*.def" -o -path "*/c++/*" \) \
+find . -type f \( -name "*.h" -o -name "*.def" -o -path "*/c++/*" \) \
   | LANG=C sort -u | while read -r header; do
     echo "  textual header \"${header}\""
   done
