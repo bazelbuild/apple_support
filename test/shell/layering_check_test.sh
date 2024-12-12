a14f82b6f77f98990d421aa13af6ca4a566d11ba
Keith Smiley <keithbsmiley@gmail.com>
2024-12-11 17:23:16 -0800

    cpp

--- test/shell/layering_check_test.sh
+++ test/shell/layering_check_test.sh
@@ -15,7 +15,7 @@ function test_bad_layering_checks() {
   ! "$bazel" test --repo_env=APPLE_SUPPORT_LAYERING_CHECK_BETA=1 -- //test/layering_check:bad_layering_check &> "$TEST_log" || fail "Expected build failure"
 
   expect_log_once "does not depend on a module exporting"
-  expect_log "test/layering_check/c.cpp:1:10: error: module @@//test/layering_check:bad_layering_check does not depend on a module exporting 'a.h'" "failed wrong layering_check"
+  expect_log "test/layering_check/c.cpp:1:10: error: module //test/layering_check:bad_layering_check does not depend on a module exporting 'a.h'" "failed wrong layering_check"
 }
 
 function test_bad_layering_checks_objc() {
