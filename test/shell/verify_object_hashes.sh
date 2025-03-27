6d43b3e6969bec21203d0d9f9ba9d9da138bdc2f
Keith Smiley <keithbsmiley@gmail.com>
2025-03-27 13:46:01 -0700

    Improve test failure message

--- test/shell/verify_object_hashes.sh
+++ test/shell/verify_object_hashes.sh
@@ -6,16 +6,16 @@ readonly binary="%{binary}s"
 output=$(ar t "$binary")
 
 if ! echo "$output" | grep -q "duplicate_a53dcba6a34993a1e83502dddc687f78.o"; then
-  echo "error: missing expected object 1" >&2
+  echo "error: missing expected object 1: $output" >&2
   exit 1
 fi
 
 if ! echo "$output" | grep -q "duplicate_a4944a8448ca496d69aa3c2b860bef74.o"; then
-  echo "error: missing expected object 2" >&2
+  echo "error: missing expected object 2: $output" >&2
   exit 1
 fi
 
 if ! echo "$output" | grep -q "SYMDEF"; then
-  echo "error: missing expected symdef" >&2
+  echo "error: missing expected symdef: $output" >&2
   exit 1
 fi
