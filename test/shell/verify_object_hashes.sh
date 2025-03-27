9e2e56d79c30358c232d97328384b75bfbb54e96
Keith Smiley <keithbsmiley@gmail.com>
2025-03-27 15:29:00 -0700

    Add another case for duplicate object basenames (#387)
    Interestingly today even non-duplicate names are symlinked and hashed.
    I think we should probably change this just for clarities sake but at
    least this encodes today's behavior

--- test/shell/verify_object_hashes.sh
+++ test/shell/verify_object_hashes.sh
@@ -15,6 +15,11 @@ if ! echo "$output" | grep -q "duplicate_a4944a8448ca496d69aa3c2b860bef74.o"; th
   exit 1
 fi
 
+if ! echo "$output" | grep -q "cc_lib_ba2b8ebd486dc5bfc4cbfacde5538c29.o"; then
+  echo "error: missing expected object 3: $output" >&2
+  exit 1
+fi
+
 if ! echo "$output" | grep -q "SYMDEF"; then
   echo "error: missing expected symdef: $output" >&2
   exit 1
