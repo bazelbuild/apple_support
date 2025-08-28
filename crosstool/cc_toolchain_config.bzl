bazelbuild/apple_support

commit 7009b77c98a67d3fea081c9db4dbcee8effc3b7e

Add support for BAZEL_CONLYOPTS, BAZEL_CXXOPTS, BAZEL_LINKOPTS (#416)
When doing multi-platform builds, using --cxxopt and friends ends up
modifying the toolchain for both OSX, and any target. rules_cc provides
environmental variables to modify the toolchain (which can be modified
through --repo_env), which get around this issue. Add support here for
them too.

Signed-off-by: Austin Schuh <austin.linux@gmail.com>

crosstool/cc_toolchain_config.bzl
@@ -177,6 +177,12 @@ please file an issue at https://github.com/bazelbuild/apple_support/issues/new
         ACTION_NAMES.cpp_link_nodeps_dynamic_library,
     ]
 
+    lto_index_actions = [
+        ACTION_NAMES.lto_index_for_executable,
+        ACTION_NAMES.lto_index_for_dynamic_library,
+        ACTION_NAMES.lto_index_for_nodeps_dynamic_library,
+    ]
+
     strip_action = action_config(
         action_name = ACTION_NAMES.strip,
         flag_sets = [
@@ -918,6 +924,14 @@ please file an issue at https://github.com/bazelbuild/apple_support/issues/new
                         expand_if_available = "user_link_flags",
                     ),
                 ],
+            ),
+            flag_set(
+                actions = all_link_actions + lto_index_actions,
+                flag_groups = ([
+                    flag_group(
+                        flags = ctx.attr.link_flags,
+                    ),
+                ] if ctx.attr.link_flags else []),
             ),
         ],
     )
@@ -1974,18 +1988,6 @@ please file an issue at https://github.com/bazelbuild/apple_support/issues/new
                 flag_groups = [flag_group(flags = ["-g"])],
                 with_features = [with_feature_set(features = ["dbg"])],
             ),
-            flag_set(
-                actions = [
-                    ACTION_NAMES.linkstamp_compile,
-                    ACTION_NAMES.cpp_compile,
-                    ACTION_NAMES.cpp_header_parsing,
-                    ACTION_NAMES.cpp_module_compile,
-                    ACTION_NAMES.cpp_module_codegen,
-                    ACTION_NAMES.lto_backend,
-                    ACTION_NAMES.clif_match,
-                ],
-                flag_groups = [flag_group(flags = ["-std=c++17"])],
-            ),
         ],
     )
 
@@ -2258,13 +2260,41 @@ please file an issue at https://github.com/bazelbuild/apple_support/issues/new
                     ACTION_NAMES.objc_compile,
                     ACTION_NAMES.objcpp_compile,
                 ],
-                flag_groups = [
+                flag_groups = ([
+                    flag_group(
+                        flags = ctx.attr.c_flags,
+                    ),
+                ] if ctx.attr.c_flags else []) + [
                     flag_group(
                         flags = ["%{user_compile_flags}"],
                         iterate_over = "user_compile_flags",
                         expand_if_available = "user_compile_flags",
                     ),
                 ],
+            ),
+            flag_set(
+                actions = [ACTION_NAMES.c_compile],
+                flag_groups = ([
+                    flag_group(
+                        flags = ctx.attr.conly_flags,
+                    ),
+                ] if ctx.attr.conly_flags else []),
+            ),
+            flag_set(
+                actions = [
+                    ACTION_NAMES.linkstamp_compile,
+                    ACTION_NAMES.cpp_compile,
+                    ACTION_NAMES.cpp_header_parsing,
+                    ACTION_NAMES.cpp_module_compile,
+                    ACTION_NAMES.cpp_module_codegen,
+                    ACTION_NAMES.lto_backend,
+                    ACTION_NAMES.clif_match,
+                ],
+                flag_groups = ([
+                    flag_group(
+                        flags = ctx.attr.cxx_flags,
+                    ),
+                ] if ctx.attr.cxx_flags else []),
             ),
         ],
     )
@@ -2714,17 +2744,21 @@ please file an issue at https://github.com/bazelbuild/apple_support/issues/new
 cc_toolchain_config = rule(
     implementation = _impl,
     attrs = {
-        "cpu": attr.string(mandatory = True),
+        "c_flags": attr.string_list(),
         "cc_wrapper": attr.label(
             allow_single_file = True,
             mandatory = True,
         ),
+        "conly_flags": attr.string_list(),
+        "cpu": attr.string(mandatory = True),
         "cxx_builtin_include_directories": attr.string_list(),
+        "cxx_flags": attr.string_list(),
         "extra_env": attr.string_dict(),
         "libtool": attr.label(
             allow_single_file = True,
             mandatory = True,
         ),
+        "link_flags": attr.string_list(),
         "module_map": attr.label(),
         "tool_paths_overrides": attr.string_dict(),
         "wrapped_clang": attr.label(

crosstool/BUILD.tpl
@@ -87,6 +87,10 @@ filegroup(
         ],
         libtool = "@build_bazel_apple_support//crosstool:exec_libtool",
         tool_paths_overrides = {%{tool_paths_overrides}},
+        c_flags = [%{c_flags}],
+        conly_flags = [%{conly_flags}],
+        cxx_flags = [%{cxx_flags}],
+        link_flags = [%{link_flags}],
         module_map = ":modulemap",
         wrapped_clang = "@build_bazel_apple_support//crosstool:exec_wrapped_clang",
         wrapped_clang_pp = "@build_bazel_apple_support//crosstool:exec_wrapped_clang_pp",

crosstool/osx_cc_configure.bzl
@@ -20,6 +20,108 @@ load(
 )
 load("@bazel_tools//tools/osx:xcode_configure.bzl", "run_xcode_locator")
 
+def get_env_var(repository_ctx, name, default = None, enable_warning = True):
+    """Find an environment variable in system path. Doesn't %-escape the value!
+
+    Args:
+      repository_ctx: The repository context.
+      name: Name of the environment variable.
+      default: Default value to be used when such environment variable is not present.
+      enable_warning: Show warning if the variable is not present.
+    Returns:
+      value of the environment variable or default.
+    """
+
+    if name in repository_ctx.os.environ:
+        return repository_ctx.os.environ[name]
+    if default != None:
+        if enable_warning:
+            auto_configure_warning("'%s' environment variable is not set, using '%s' as default" % (name, default))
+        return default
+    auto_configure_fail("'%s' environment variable is not set" % name)
+    return None
+
+def auto_configure_fail(msg):
+    """Output failure message when auto configuration fails."""
+    red = "\033[0;31m"
+    no_color = "\033[0m"
+    fail("\n%sAuto-Configuration Error:%s %s\n" % (red, no_color, msg))
+
+def auto_configure_warning(msg):
+    """Output warning message during auto configuration."""
+    yellow = "\033[1;33m"
+    no_color = "\033[0m"
+
+    # buildifier: disable=print
+    print("\n%sAuto-Configuration Warning:%s %s\n" % (yellow, no_color, msg))
+
+def split_escaped(string, delimiter):
+    """Split string on the delimiter unless %-escaped.
+
+    Examples:
+      Basic usage:
+        split_escaped("a:b:c", ":") -> [ "a", "b", "c" ]
+
+      Delimiter that is not supposed to be splitten on has to be %-escaped:
+        split_escaped("a%:b", ":") -> [ "a:b" ]
+
+      Literal % can be represented by escaping it as %%:
+        split_escaped("a%%b", ":") -> [ "a%b" ]
+
+      Consecutive delimiters produce empty strings:
+        split_escaped("a::b", ":") -> [ "a", "", "", "b" ]
+
+    Args:
+      string: The string to be split.
+      delimiter: Non-empty string not containing %-sign to be used as a
+          delimiter.
+
+    Returns:
+      A list of substrings.
+    """
+    if delimiter == "":
+        fail("Delimiter cannot be empty")
+    if delimiter.find("%") != -1:
+        fail("Delimiter cannot contain %-sign")
+
+    i = 0
+    result = []
+    accumulator = []
+    length = len(string)
+    delimiter_length = len(delimiter)
+
+    if not string:
+        return []
+
+    # Iterate over the length of string since Starlark doesn't have while loops
+    for _ in range(length):
+        if i >= length:
+            break
+        if i + 2 <= length and string[i:i + 2] == "%%":
+            accumulator.append("%")
+            i += 2
+        elif (i + 1 + delimiter_length <= length and
+              string[i:i + 1 + delimiter_length] == "%" + delimiter):
+            accumulator.append(delimiter)
+            i += 1 + delimiter_length
+        elif i + delimiter_length <= length and string[i:i + delimiter_length] == delimiter:
+            result.append("".join(accumulator))
+            accumulator = []
+            i += delimiter_length
+        else:
+            accumulator.append(string[i])
+            i += 1
+
+    # Append the last group still in accumulator
+    result.append("".join(accumulator))
+    return result
+
+def get_starlark_list(values):
+    """Convert a list of string into a string that can be passed to a rule attribute."""
+    if not values:
+        return ""
+    return "\"" + "\",\n    \"".join(values) + "\""
+
 def _get_escaped_xcode_cxx_inc_directories(repository_ctx, xcode_toolchains):
     """Compute the list of default C++ include paths on Xcode-enabled darwin.
 
@@ -112,13 +214,43 @@ def configure_osx_toolchain(repository_ctx):
         escaped_cxx_include_directories.append(("            \"%s\"," % path))
     if xcodeloc_err:
         escaped_cxx_include_directories.append("            # Error: " + xcodeloc_err)
+
+    conly_opts = split_escaped(get_env_var(
+        repository_ctx,
+        "BAZEL_CONLYOPTS",
+        "",
+        False,
+    ), ":")
+    c_opts = split_escaped(get_env_var(
+        repository_ctx,
+        "BAZEL_COPTS",
+        "",
+        False,
+    ), ":")
+    cxx_opts = split_escaped(get_env_var(
+        repository_ctx,
+        "BAZEL_CXXOPTS",
+        "-std=c++17",
+        False,
+    ), ":")
+    link_opts = split_escaped(get_env_var(
+        repository_ctx,
+        "BAZEL_LINKOPTS",
+        "",
+        False,
+    ), ":")
+
     repository_ctx.template(
         "BUILD",
         build_template,
         {
+            "%{c_flags}": get_starlark_list(c_opts),
+            "%{conly_flags}": get_starlark_list(conly_opts),
             "%{cxx_builtin_include_directories}": "\n".join(escaped_cxx_include_directories),
+            "%{cxx_flags}": get_starlark_list(cxx_opts),
             "%{features}": "\n".join(['"{}"'.format(x) for x in features]),
             "%{layering_check_modulemap}": "\"@build_bazel_apple_support//crosstool:generate_layering_check_modulemap\"," if enable_layering_check else "",
+            "%{link_flags}": get_starlark_list(link_opts),
             "%{placeholder_modulemap}": "\"@build_bazel_apple_support//crosstool:module.modulemap\"" if enable_layering_check else "None",
             "%{tool_paths_overrides}": ",\n            ".join(
                 ['"%s": "%s"' % (k, v) for k, v in tool_paths.items()],

crosstool/setup_internal.bzl
@@ -63,6 +63,10 @@ apple_cc_autoconf = repository_rule(
         "USE_CLANG_CL",  # Kept as a hack for those who rely on this invaliding the toolchain
         "USER",  # Used to allow paths for custom toolchains to be used by C* compiles
         "XCODE_VERSION",  # Force re-computing the toolchain by including the current Xcode version info in an env var
+        "BAZEL_COPTS",
+        "BAZEL_CONLYOPTS",
+        "BAZEL_CXXOPTS",
+        "BAZEL_LINKOPTS",
     ],
     implementation = _apple_cc_autoconf_impl,
     configure = True,

commit 8f6e19955cf8dfb4f0a4c6cd60464cc570a5e6bd

Fix cxx_flags order
Default flags should come before flags passed with `--copt` or `copts = []`

Broken by 7009b77c98a67d3fea081c9db4dbcee8effc3b7e fixes https://github.com/bazelbuild/apple_support/issues/422

crosstool/cc_toolchain_config.bzl
@@ -2291,13 +2291,7 @@ please file an issue at https://github.com/bazelbuild/apple_support/issues/new
                     flag_group(
                         flags = ctx.attr.c_flags,
                     ),
-                ] if ctx.attr.c_flags else []) + [
-                    flag_group(
-                        flags = ["%{user_compile_flags}"],
-                        iterate_over = "user_compile_flags",
-                        expand_if_available = "user_compile_flags",
-                    ),
-                ],
+                ] if ctx.attr.c_flags else []),
             ),
             flag_set(
                 actions = [ACTION_NAMES.c_compile],
@@ -2322,6 +2316,27 @@ please file an issue at https://github.com/bazelbuild/apple_support/issues/new
                         flags = ctx.attr.cxx_flags,
                     ),
                 ] if ctx.attr.cxx_flags else []),
+            ),
+            flag_set(
+                actions = [
+                    ACTION_NAMES.assemble,
+                    ACTION_NAMES.preprocess_assemble,
+                    ACTION_NAMES.c_compile,
+                    ACTION_NAMES.cpp_compile,
+                    ACTION_NAMES.cpp_header_parsing,
+                    ACTION_NAMES.cpp_module_compile,
+                    ACTION_NAMES.cpp_module_codegen,
+                    ACTION_NAMES.linkstamp_compile,
+                    ACTION_NAMES.objc_compile,
+                    ACTION_NAMES.objcpp_compile,
+                ],
+                flag_groups = [
+                    flag_group(
+                        flags = ["%{user_compile_flags}"],
+                        iterate_over = "user_compile_flags",
+                        expand_if_available = "user_compile_flags",
+                    ),
+                ],
             ),
         ],
     )

test/compiling_tests.bzl
@@ -33,6 +33,7 @@ def compiling_test_suite(name):
         tags = [name],
         expected_argv = [
             "-fdebug-prefix-map=__BAZEL_EXECUTION_ROOT__=.",
+            "-std=c++17 -std=c++20",
         ],
         not_expected_argv = [
             "-DNS_BLOCK_ASSERTIONS=1",

test/test_data/BUILD
@@ -28,6 +28,7 @@ cc_binary(
 cc_library(
     name = "cc_main",
     srcs = ["main.cc"],
+    copts = ["-std=c++20"],
     tags = TARGETS_UNDER_TEST_TAGS,
 )
 