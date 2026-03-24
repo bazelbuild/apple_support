38285aaf9e5498e5fbffe17bf05c767add95d906
Keith Smiley <keithbsmiley@gmail.com>
2026-03-24 14:22:58 -0700

    improve

--- test/shell/coverage_test.sh
+++ test/shell/coverage_test.sh
@@ -12,6 +12,7 @@ llvm_cov=$(xcrun -f llvm-cov)
 
 function test_llvm_lcov_coverage() {
   "$bazel" coverage \
+    --experimental_fetch_all_coverage_outputs \
     --experimental_generate_llvm_lcov \
     --features=llvm_coverage_map_format \
     --instrument_test_targets \
@@ -19,10 +20,11 @@ function test_llvm_lcov_coverage() {
     --test_env=LLVM_COV="$llvm_cov" \
     --test_env=VERBOSE_COVERAGE=1 \
     --test_output=all \
+    --nocache_test_results \
     //test/test_data:c_test &>"$TEST_log" \
     || fail "bazel coverage failed"
 
-  coverage_file="bazel-out/_coverage/_coverage_report.dat"
+  coverage_file=bazel-out/_coverage/_coverage_report.dat
   cat "$coverage_file"
   [[ -f "$coverage_file" ]] || fail "coverage.dat not found at $coverage_file"
   grep -q "^SF:test/test_data/test_lib.c" "$coverage_file" || fail "coverage.dat does not contain source file entries"
@@ -30,17 +32,23 @@ function test_llvm_lcov_coverage() {
 }
 
 function test_llvm_profdata_coverage() {
+  # experimental_split_coverage_postprocessing is required for the profdata to stick around
   "$bazel" coverage \
+    --experimental_fetch_all_coverage_outputs \
     --experimental_generate_llvm_lcov=false \
     --features=llvm_coverage_map_format \
     --instrument_test_targets \
     --test_env=LLVM_PROFDATA="$llvm_profdata" \
     --test_env=VERBOSE_COVERAGE=1 \
     --test_output=all \
+    --nocache_test_results \
+    --experimental_split_coverage_postprocessing=false \
     //test/test_data:c_test &>"$TEST_log" \
     || fail "bazel coverage failed"
 
-  coverage_file="bazel-out/_coverage/_coverage_report.dat"
+  profdata=bazel-testlogs/test/test_data/c_test/_coverage/_cc_coverage.profdata
+  [[ -f "$profdata" ]] || fail "_cc_coverage.profdata not found at $profdata"
+  coverage_file=bazel-out/_coverage/_coverage_report.dat
   [[ -f "$coverage_file" ]] || fail "coverage.dat not found at $coverage_file"
   grep -q "^SF:test/test_data/test_lib.c" "$coverage_file" || fail "coverage.dat does not contain source file entries"
   # NOTE: This format seems to not have actually useful coverage, which appears to be coming from lcov-merger in bazel
