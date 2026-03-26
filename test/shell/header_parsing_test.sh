0cba19e8ab1e5469ccfc30e57113179ba0ad11cd
Keith Smiley <keithbsmiley@gmail.com>
2026-03-26 12:28:28 -0700

    test

--- test/shell/header_parsing_test.sh
+++ test/shell/header_parsing_test.sh
@@ -7,19 +7,4 @@ source "$script_path"/unittest.bash
 
 bazel="${BAZEL:-bazel}"
 
-function test_good_header_parsing() {
-  "$bazel" test --announce_rc --process_headers_in_dependencies -- //test/header_parsing/...
-  "$bazel" test --announce_rc --process_headers_in_dependencies -- //test/header_parsing/... &>"$TEST_log"
-}
-
-function test_bad_header_parsing() {
-  ! "$bazel" test --process_headers_in_dependencies -- //test/header_parsing:invalid_header &> "$TEST_log" || fail "Expected build failure"
-  expect_log "test/header_parsing/invalid_header.h:2:1: error: unknown type name 'uint8_t'"
-}
-
-function test_bad_header_parsing_objc() {
-  ! "$bazel" test --process_headers_in_dependencies -- //test/header_parsing:invalid_header_objc &> "$TEST_log" || fail "Expected build failure"
-  expect_log "test/header_parsing/invalid_header.h:2:1: error: unknown type name 'uint8_t'"
-}
-
-run_suite "header_parsing tests"
+"$bazel" test --announce_rc --process_headers_in_dependencies -- //test/header_parsing/...
