5bcfd6aca70298d3e2a5f8f3d4cdb2c9f731b45d
Keith Smiley <keithbsmiley@gmail.com>
2026-03-26 12:51:31 -0700

    assertions

--- test/shell/verify_object_hashes.sh
+++ test/shell/verify_object_hashes.sh
@@ -5,17 +5,18 @@ set -euo pipefail
 readonly binary="%{binary}s"
 output=$(ar t "$binary")
 
-if ! echo "$output" | grep -q "duplicate_3e0a6abe8af91e33.o"; then
+# NOTE: The first character of the transition hash is enough to verify they're unique
+if ! echo "$output" | grep -q "duplicate_f.*.o"; then
   echo "error: missing expected object 1: $output" >&2
   exit 1
 fi
 
-if ! echo "$output" | grep -q "duplicate_f47fea066e30c006.o"; then
+if ! echo "$output" | grep -q "duplicate_3.*.o"; then
   echo "error: missing expected object 2: $output" >&2
   exit 1
 fi
 
-if ! echo "$output" | grep -q "cc_lib_bd9e12793f4560a2.o"; then
+if ! echo "$output" | grep -q "cc_lib_c.*.o"; then
   echo "error: missing expected object 3: $output" >&2
   exit 1
 fi
