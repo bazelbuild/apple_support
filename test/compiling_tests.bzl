bazelbuild/apple_support

commit 643fa0f4fb41a2cb7533f142c089244e3a83679d

Remove framework_paths from implies (#523)
This also uncovered that these link specific flags have been dead for
years. Those are just piped through normal linkopts instead

crosstool/cc_toolchain_config.bzl
@@ -243,7 +243,6 @@ please file an issue at https://github.com/bazelbuild/apple_support/issues/new
         action_name = ACTION_NAMES.objc_compile,
         enabled = True,
         implies = [
-            "framework_paths",
             "user_compile_flags",
             "unfiltered_compile_flags",
         ],
@@ -357,7 +356,6 @@ please file an issue at https://github.com/bazelbuild/apple_support/issues/new
             ),
         ],
         implies = [
-            "framework_paths",
             "user_compile_flags",
             "unfiltered_compile_flags",
         ],
@@ -437,7 +435,6 @@ please file an issue at https://github.com/bazelbuild/apple_support/issues/new
             ),
         ],
         implies = [
-            "framework_paths",
             "strip_debug_symbols",
         ],
         tools = [
@@ -991,15 +988,11 @@ please file an issue at https://github.com/bazelbuild/apple_support/issues/new
     )
 
     framework_paths_feature = feature(
-        name = "framework_paths",
+        name = "__framework_paths",
+        enabled = True,
         flag_sets = [
             flag_set(
                 actions = [
-                    ACTION_NAMES.preprocess_assemble,
-                    ACTION_NAMES.c_compile,
-                    ACTION_NAMES.cpp_compile,
-                    ACTION_NAMES.cpp_header_parsing,
-                    ACTION_NAMES.cpp_module_compile,
                     ACTION_NAMES.objc_compile,
                     ACTION_NAMES.objcpp_compile,
                 ],
@@ -1007,23 +1000,6 @@ please file an issue at https://github.com/bazelbuild/apple_support/issues/new
                     flag_group(
                         flags = ["-F%{framework_include_paths}"],
                         iterate_over = "framework_include_paths",
-                    ),
-                ],
-            ),
-            flag_set(
-                actions = [ACTION_NAMES.objc_executable],
-                flag_groups = [
-                    flag_group(
-                        flags = ["-F%{framework_paths}"],
-                        iterate_over = "framework_paths",
-                    ),
-                    flag_group(
-                        flags = ["-framework", "%{framework_names}"],
-                        iterate_over = "framework_names",
-                    ),
-                    flag_group(
-                        flags = ["-weak_framework", "%{weak_framework_names}"],
-                        iterate_over = "weak_framework_names",
                     ),
                 ],
             ),

test/compiling_tests.bzl
@@ -109,9 +109,7 @@ def compiling_test_suite(name):
     default_test(
         name = "{}_framework_include_paths_test".format(name),
         tags = [name],
-        expected_argv = [
-            "-Ftest/test_data/frameworks",
-        ],
+        expected_argv = ["-Ftest/test_data/frameworks"],
         mnemonic = "ObjcCompile",
         target_under_test = "//test/test_data:objc_framework_includes_lib",
     )

test/test_data/BUILD
@@ -93,6 +93,23 @@ objc_library(
 )
 
 objc_library(
+    name = "objc_library_sdk_frameworks",
+    srcs = ["main.m"],
+    # SDKs picked at random but shouldn't overlap with things that might be linked naturally
+    sdk_frameworks = ["CalendarStore"],
+    tags = TARGETS_UNDER_TEST_TAGS,
+    weak_sdk_frameworks = ["Accounts"],
+)
+
+starlark_apple_binary(
+    name = "macos_binary_with_sdk_frameworks",
+    minimum_os_version = "13.0",
+    platform_type = "macos",
+    tags = TARGETS_UNDER_TEST_TAGS,
+    deps = [":objc_library_sdk_frameworks"],
+)
+
+objc_library(
     name = "objc_no_arc_lib",
     non_arc_srcs = ["objc_lib.m"],
     tags = TARGETS_UNDER_TEST_TAGS,

test/linking_tests.bzl
@@ -179,6 +179,19 @@ def linking_test_suite(name):
         target_under_test = "//test/test_data:ios_binary",
     )
 
+    default_test(
+        name = "{}_objc_link_sdk_frameworks_test".format(name),
+        tags = [name],
+        expected_argv = [
+            "-framework",
+            "CalendarStore",
+            "-weak_framework",
+            "Accounts",
+        ],
+        mnemonic = "ObjcLink",
+        target_under_test = "//test/test_data:macos_binary_with_sdk_frameworks",
+    )
+
     dsym_test(
         name = "{}_generate_dsym_test".format(name),
         tags = [name],

commit d7102ba315939ee54efa539677e2185124eea41b

Fix -F flags to C++ compiles
I broke this in 643fa0f4fb41a2cb7533f142c089244e3a83679d by assuming the
fact that the features weren't `implies`ed by cpp compile meant they
weren't used in that action

crosstool/cc_toolchain_config.bzl
@@ -964,6 +964,11 @@ please file an issue at https://github.com/bazelbuild/apple_support/issues/new
         flag_sets = [
             flag_set(
                 actions = [
+                    ACTION_NAMES.preprocess_assemble,
+                    ACTION_NAMES.c_compile,
+                    ACTION_NAMES.cpp_compile,
+                    ACTION_NAMES.cpp_header_parsing,
+                    ACTION_NAMES.cpp_module_compile,
                     ACTION_NAMES.objc_compile,
                     ACTION_NAMES.objcpp_compile,
                 ],

test/compiling_tests.bzl
@@ -198,10 +198,18 @@ def compiling_test_suite(name):
     )
 
     default_test(
-        name = "{}_framework_include_paths_test".format(name),
+        name = "{}_framework_include_paths_objc_test".format(name),
         tags = [name],
         expected_argv = ["-Ftest/test_data/frameworks"],
         mnemonic = "ObjcCompile",
+        target_under_test = "//test/test_data:objc_framework_includes_lib",
+    )
+
+    default_test(
+        name = "{}_framework_include_paths_cpp_test".format(name),
+        tags = [name],
+        expected_argv = ["-Ftest/test_data/frameworks"],
+        mnemonic = "CppCompile",
         target_under_test = "//test/test_data:objc_framework_includes_lib",
     )
 

test/test_data/BUILD
@@ -95,7 +95,10 @@ framework_includes_lib(
 
 objc_library(
     name = "objc_framework_includes_lib",
-    srcs = ["objc_lib.m"],
+    srcs = [
+        "objc_lib.cpp",
+        "objc_lib.m",
+    ],
     tags = TARGETS_UNDER_TEST_TAGS,
     deps = [":framework_includes_dep"],
 )

test/test_data/objc_lib.cpp
@@ -1,0 +1,1 @@
+int cpp_function() { return 1; }